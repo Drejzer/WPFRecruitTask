54151e790dd066e3a932e67143b57221695393af
Drejzer <drejzer@gmail.com>
2020-12-09 23:33:55 +0100

    Add project files.

---
+++ TaskUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace TaskUI.ViewModels
+    {
+    public class ShellViewModel:Screen
+        {
+        private string _name;
+        public string Name
+            {
+            get
+                {
+                return _name;
+                }
+            set
+                {
+                _name = value;
+                NotifyOfPropertyChange(()=>Name);
+                }
+            }
+        }
+    }
